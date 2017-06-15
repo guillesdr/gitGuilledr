2d4775c39e8eb1c08e1a29936a5ff3904d81c25a
Guille <gdella@dgrcorrientes.gov.ar>
2017-06-15 11:40:12 -0300

    Primer commit

---
+++ rentas/src/rentas/empleado.java
@@ -0,0 +1,35 @@
+package rentas;
+
+public class empleado {
+
+	private String nombre;
+	private String apellido;
+	private String tipoDoc;
+	private int nroDoc;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTipoDoc() {
+		return tipoDoc;
+	}
+	public void setTipoDoc(String tipoDoc) {
+		this.tipoDoc = tipoDoc;
+	}
+	public int getNroDoc() {
+		return nroDoc;
+	}
+	public void setNroDoc(int nroDoc) {
+		this.nroDoc = nroDoc;
+	}
+	
+}
