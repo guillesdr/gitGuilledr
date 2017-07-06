446ccf129bc3a9072edbf22fb98555eb42071cbe
guillesdr <guillesdr@gmail.com>
2017-07-06 09:31:27 -0300

    Modificaciones Impuesto

--- rentas/src/rentas/impuesto.java
+++ rentas/src/rentas/impuesto.java
@@ -4,9 +4,16 @@ public class impuesto {
 
 	private int id;
 	private String descripcion;
+	private double alicuota;
 	
 	public int getId() {
 		return id;
+	}
+	public double getAlicuota() {
+		return alicuota;
+	}
+	public void setAlicuota(double alicuota) {
+		this.alicuota = alicuota;
 	}
 	public void setId(int id) {
 		this.id = id;
